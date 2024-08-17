7c488d5468f9df006dd7a5b564a387dd86ff0229
KHalilZiadeh <khalilziadeh98@gmail.com>
2024-08-18 01:23:28 +0300

    Neovim typos

--- lua/user/options.lua
+++ lua/user/options.lua
@@ -27,12 +27,12 @@ local options = {
   cursorline = true,                       -- highlight the current line
   number = true,                           -- set numbered lines
   relativenumber = true,                   -- set relative numbered lines
-  numberwidth = 4,                         -- set number column width to 2 {default 4}
-  signcolumn = "yes",                      -- always show the sign column, otherwise it would shift the text each time
+  numberwidth = 3,                         -- set number column width to 2 {default 4}
+  signcolumn = "no",                      -- always show the sign column, otherwise it would shift the text each time
   wrap = false,                            -- display lines as one long line
   scrolloff = 8,                           -- is one of my fav
   sidescrolloff = 8,
-  guifont = "monospace:h17",               -- the font used in graphical neovim applications
+  --[[ guifont = "monospace:h17",               -- the font used in graphical neovim applications ]]
 }
 
 vim.opt.shortmess:append "c"
